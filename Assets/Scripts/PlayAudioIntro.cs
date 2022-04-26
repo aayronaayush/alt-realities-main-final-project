919652eea4817f8e6b7af7df50e82e25c5949bf8
aayush <aayush@mentalvr.ca>
2022-04-26 14:54:50 +0400

    add intro sound placeholder

--- Assets/Scripts/PlayAudioIntro.cs
+++ Assets/Scripts/PlayAudioIntro.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IntroSoundController : MonoBehaviour
+public class PlayAudioIntro : MonoBehaviour
 {
+    AudioSource audioSource; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
