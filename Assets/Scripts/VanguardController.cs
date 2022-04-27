171aab2a2ef5d7c641b2eac48d9f4529ea191217
yahiabeethoven <ybt214@nyu.edu>
2022-04-28 02:46:14 +0400

    Set up robots for motion, no script yet

---
+++ Assets/Scripts/VanguardController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanguardController : MonoBehaviour
+{
+	Animator animator;
+	NavMeshAgent agent;
+
+	int isFlyingHash;
+	int velocityXHash;
+	int velocityZHash;
+
+	private void Awake()
+	{
+		animator = GetComponent<Animator>();
+		agent = GetComponent<NavMeshAgent>();
+
+		isFlyingHash = Animator.StringToHash("isFlying");
+		velocityXHash = Animator.StringToHash("velocityX");
+		velocityZHash = Animator.StringToHash("velocityZ");
+	}
+
+	private void Update()
+	{
+		Vector3 velocity = agent.velocity;
+
+		bool isMoving = velocity.magnitude > 0.01f && agent.remainingDistance > agent.radius;
+
+		animator.SetBool(isFlyingHash, isMoving);
+
+		velocity = transform.InverseTransformVector(velocity);
+		//		transform.position += transform.forward * Time.deltaTime;
+		animator.SetFloat(velocityXHash, velocity.x);
+		animator.SetFloat(velocityZHash, velocity.z);
+	}
+}
+}
